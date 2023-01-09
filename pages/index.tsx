24dafab449bfcf03bc60f6bfffb3e4c6d8583706
thanh.nguyen99.futa <thanh.nguyen99@futa.vn>
2023-01-09 10:53:10 +0700

    change footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{"Em Thanh"}
+          Powered by {"nnthanh01061999@gmail.com"}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
